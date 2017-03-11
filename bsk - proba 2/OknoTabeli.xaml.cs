b3d00e8376a25b43a3669b0f81bc1a2a7d71efaf
tomasz152 <tomaszdlugokinski@gmail.com>
2017-03-12 00:10:40 +0100

    Częściowe uzupełnienie okien użytkownika

---
+++ bsk - proba 2/OknoTabeli.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace bsk___proba_2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OknoTabeli.xaml
+    /// </summary>
+    public partial class OknoTabeli : Window
+    {
+        public OknoTabeli()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- bsk - proba 2/UserWindow.xaml.cs
+++ bsk - proba 2/UserWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace bsk___proba_2
 {
@@ -22,6 +10,11 @@ namespace bsk___proba_2
         public UserWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            OknoTabeli win2 = new OknoTabeli();
+            win2.Show();
         }
     }
 }
